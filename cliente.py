e2285140515d20828359a23c2dee3d96a9be99a9
Adrian Robles <C15460218@colima.tecnm.mx>
2020-08-08 10:43:55 -0500

    commit 20200808

--- cliente.py
+++ cliente.py
@@ -9,12 +9,12 @@ Autor: tomado de https://realpython.com/python-sockets/
 
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+HOST = '192.168.0.102'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
+    s.sendall(b'Hola Mundo Peludo')
     data = s.recv(1024)
 
 print('Received', repr(data))
